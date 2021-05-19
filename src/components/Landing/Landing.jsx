1a1d4c23a2f0815db760395b525cb38f92b2517a
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2021-05-18 21:18:03 -0700

    -trimmed fat -added stylesheets

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProductBar from './ProductBar/ProductBar.jsx'
+
+const Landing = () => {
+  return (
+    <div className="landing">
+    <ProductBar/>
+    </div>
+  )
+}
+
+
+export default Landing;
