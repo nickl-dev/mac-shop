676de048bf2895d492d1b76f75613e5f818eae21
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2021-07-17 12:27:24 -0700

    -added animation for Home header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,14 +11,29 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__content">
-        <Link to="/" className="footer__logo">
-          <FontAwesomeIcon
-            icon={["fab", "apple"]}
-            className="footer__logo"
-            size="lg"
-          />
-        </Link>
-        Mac
+        <div className="footer__header">
+          <Link to="/" className="footer__logo">
+            <FontAwesomeIcon
+              icon={["fab", "apple"]}
+              className="footer__logo"
+              size="lg"
+            />
+          </Link>
+          MacShop
+        </div>
+
+        <div className="footer__locale">
+          More ways to shop:
+          <a href="https://www.apple.com/ca/mac/" className="footer__anchor">Find an Apple Store</a>
+           or
+          <a href="https://www.apple.com/ca/mac/" className="footer__anchor">other retailer</a>
+          near you.
+          <span>Or call 1-800-MAC-SHOP.</span>
+        </div>
+
+        <div className="footer__legal">
+          Copyright ©2021 Apple Inc. All rights reserved.
+        </div>
       </div>
     </footer>
   )
